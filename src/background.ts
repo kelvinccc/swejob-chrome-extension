cfdb87bcb07c18f2eb338538f48e972272c2c799
Kelvin Chung <kelvinc2016@gmail.com>
2022-09-14 22:42:54 -0700

    background.ts: (+) storage sync setter and getter methods setStorageItem(item : Object) => Promise getStorageItem(items : string | string[]) => Promise
    (+) OnMessage Listener for saving info action
    
    popup.ts:
    (+) parseForm() => ResumeInfo
    (+) hasKey(obj, key)
    (+) saveInfo(resumeInfo)
    
    Added type check for chrome extension API

--- src/background.ts
+++ src/background.ts
@@ -1,13 +1,40 @@
-// Retrieves object that contains resume info
-async function infoObject () {
-  return chrome.storage.sync.get('infoObject').then((items) => {
-    if (Object.keys(items).length === 0) {
-      // Form has never been filled in yet,
-      // Display pop up to fill in
-      return {}
-    }
-    return items
+const DEBUG = false
+
+chrome.runtime.onMessage.addListener(async (message, sender, sendResponse) => {
+  if (message.action === 'saveInfo') {
+    await setStorageItem(message.resumeInfo)
+    sendResponse(true)
+    return true // return true to indicate to have sendResponse be called asynchronously
+  }
+})
+
+type StorageItem = {}
+
+const setStorageItem = async (item : StorageItem) => {
+  return new Promise((resolve, reject) => {
+    chrome.storage.sync.set((item), () => {
+      // Pass any observed errors down the promise chain.
+      if (chrome.runtime.lastError) {
+        return reject(chrome.runtime.lastError)
+      }
+      return resolve(true)
+    })
   })
 }
 
-console.log(infoObject())
+const getStorageItem = async (keys: string | string[]) => {
+  return new Promise((resolve, reject) => {
+    chrome.storage.sync.get(keys, (items) => {
+      if (DEBUG) {
+        console.log(items)
+        console.log(keys)
+      }
+      // Pass any observed errors down the promise chain.
+      if (chrome.runtime.lastError) {
+        return reject(chrome.runtime.lastError)
+      }
+      // Pass the data retrieved from storage down the promise chain.
+      return resolve(items)
+    })
+  })
+}
--- src/popup.ts
+++ src/popup.ts
@@ -3,8 +3,49 @@ const submit = document.getElementById('submit')
 if (submit !== null) {
   submit.onclick = (event: Event) => {
     event.preventDefault()
-    console.log(event)
-    console.log('hello')
+    // Parse each input and...
+    saveInfo(parseForm())
+    // Save info to sync storage API
   }
 }
-console.log('hello')
+
+let ResumeInfo : {
+  fname: string,
+  lname: string,
+  email: string,
+  phone: string,
+  linkedin: string
+}
+
+// Saves ResumeInfo object to sync storage
+const saveInfo = async (resumeInfo : typeof ResumeInfo) => {
+  console.log(resumeInfo)
+  chrome.runtime.sendMessage({ action: 'saveInfo', resumeInfo })
+}
+
+// Takes each input and builds a ResumeInfo object
+const parseForm = () => {
+  const resumeInfo = {
+    fname: '',
+    lname: '',
+    email: '',
+    phone: '',
+    linkedin: ''
+  }
+
+  const inputs = document.querySelectorAll('input')
+  for (const input of inputs) {
+    if (input.type !== 'submit' && hasKey(resumeInfo, input.name)) {
+      resumeInfo[input.name] = input.value
+    } else {
+      // return some error here
+    }
+  }
+
+  return resumeInfo
+}
+
+// Typescript workaround for key indexing objects to make sure compiler doesn't complain
+function hasKey<O> (obj : O, key : PropertyKey) : key is keyof O {
+  return key in obj
+}
