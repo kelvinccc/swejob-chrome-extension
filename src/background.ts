2ba3c5b103948629f3686325bec52b36dfd54f7d
Kelvin Chung <kelvinc2016@gmail.com>
2022-10-08 21:12:43 -0700

    Fixed routing & session storage CRUD errors

--- src/background.ts
+++ src/background.ts
@@ -1,4 +1,4 @@
-const DEBUG = false
+const DEBUG = true
 
 // Type declaration for the object representing information to input onto resume
 type ResumeI = {
@@ -9,46 +9,34 @@ type ResumeI = {
   'linkedin': string
 }
 
-// empty Template that represents default values for the ResumeInfo Object
-const INFO_ITEMS : ResumeI = {
+type db = {resumeInfo : ResumeI}
+
+// current state of resumeInfo
+let resumeInfo : ResumeI = {
   fname: '',
   lname: '',
   email: '',
   phone: '',
   linkedin: ''
 }
-
-let info = {
-  fname: '',
-  lname: '',
-  email: '',
-  phone: '',
-  linkedin: ''
-}
-
-chrome.runtime.onStartup.addListener(async () => {
-  const resumeInfo = await getStorageItem(info as ResumeI)
-  if (typeof resumeInfo === 'undefined') {
-    await setStorageItem(INFO_ITEMS)
-  } else {
-    info = resumeInfo as ResumeI
-  }
-})
 
 chrome.runtime.onMessage.addListener(async (message, sender, sendResponse) => {
   if (message.action === 'saveInfo') {
+    console.log(message.resumeInfo)
     await setStorageItem(message.resumeInfo)
     await setResumeInfo(message.resumeInfo)
     sendResponse(true)
     return true // return true to indicate to have sendResponse be called asynchronously
   } else if (message.action === 'getInfo') {
-    sendResponse(info)
+    console.log('message recieved to get info...')
+    sendResponse(resumeInfo)
     return true
   }
 })
 
 type StorageItem = {}
 
+// Save resumeInfo to storage
 const setStorageItem = async (item : StorageItem) => {
   return new Promise((resolve, reject) => {
     chrome.storage.sync.set((item), () => {
@@ -61,24 +49,58 @@ const setStorageItem = async (item : StorageItem) => {
   })
 }
 
-const getStorageItem = async (keys: string | string[] | ResumeI) => {
-  return new Promise((resolve, reject) => {
+// Get resumeInfo from storage
+const getStorageItem = async (keys: string | string[] | ResumeI | null) => {
+  return new Promise((resolve : (res : db | PromiseLike<db>) => void, reject) => {
     chrome.storage.sync.get(keys, (items) => {
       if (DEBUG) {
+        console.log('items, keys')
         console.log(items)
         console.log(keys)
       }
+
       // Pass any observed errors down the promise chain.
       if (chrome.runtime.lastError) {
         return reject(chrome.runtime.lastError)
       }
       // Pass the data retrieved from storage down the promise chain.
-      return resolve(items)
+      return resolve(items as db)
     })
   })
 }
+// Initialization - get resumeinfo object from storage
+// if it has not been initialized, set it to initial state (defined as the info object above)
+getStorageItem('resumeInfo').then((res) => {
+  console.log('getting resume info from sync. heres the res : ')
+  console.log(res)
+  if ('resumeInfo' in res) {
+    console.log('got resume info! : ')
+    resumeInfo = res.resumeInfo
+    console.log(resumeInfo)
+  } else {
+    console.log('doesnt exist yet')
+    initStorageWithResumeInfo()
+  }
+  return res
+}).catch((rej) => {
+  console.log('failed to retrieve resumeInfo from sync storage')
+  return rej
+})
 
-// update resumeInfo object
-const setResumeInfo = async (resumeInfo : ResumeI) => {
-  info = resumeInfo
+const initStorageWithResumeInfo = async () => {
+  await setStorageItem({ resumeInfo })
+  if (DEBUG) {
+    await debugInfo()
+  }
+  return true
+}
+
+// update resumeInfo object for current session
+const setResumeInfo = async (info : ResumeI) => {
+  resumeInfo = info
+}
+
+const debugInfo = async () => {
+  console.log('checking db state...')
+  await getStorageItem(null)
 }
--- src/popup.ts
+++ src/popup.ts
@@ -11,38 +11,48 @@ type ResumeInfo = {
   'linkedin': string
 }
 
-const init = () => {
-  if (save !== null) {
-    save.onclick = (event: Event) => {
-      event.preventDefault()
-      handleSaveButton(event)
+const order = ['fname', 'lname', 'email', 'phone', 'linkedin']
+
+const parseInfo = (resumeInfo : ResumeInfo) => {
+  console.log('parseInfo...')
+  console.log(resumeInfo)
+  console.log(Object.entries(resumeInfo))
+  let isResumeInfoEmpty = true
+  for (const [key, value] of Object.entries(resumeInfo)) {
+    console.log(key)
+    console.log(value.length)
+    if (value.length !== 0) {
+      isResumeInfoEmpty = false
+      break
     }
   }
-  if (cornerButton !== null) {
-    cornerButton.onclick = (event: Event) => {
-      event.preventDefault()
-      handleCloseButton(event)
-    }
+
+  if (!isResumeInfoEmpty) {
+    console.log('displaying info cuz resumeinfo is not empty')
+    displayInfo(resumeInfo, () => generateInfoHTML(resumeInfo, 'info'))
+    changeCornerButton('edit')
+  } else {
+    console.log('displaying form cuz resumeInfo is empty')
+    displayInfo(resumeInfo, () => generateFormHTML(resumeInfo, 'form'))
+    changeCornerButton('close')
   }
 }
-
-init()
 
 /*
   Saves ResumeInfo object to sync storage && updates resumeInfo object in background.js
 */
 const saveInfo = async (resumeInfo : ResumeInfo) => {
-  chrome.runtime.sendMessage({ action: 'saveInfo', resumeInfo })
+  await chrome.runtime.sendMessage({ action: 'saveInfo', resumeInfo })
+  console.log('saving info')
+  await getInfo()
 }
 
 /*
   Grab current state of resumeInfo from background.js
 */
 const getInfo = async () => {
-  const info = await chrome.runtime.sendMessage({ action: 'getInfo' })
-  return info
+  return await chrome.runtime.sendMessage({ action: 'getInfo' })
 }
-
 // Takes each input and builds a ResumeInfo object
 const parseForm = () => {
   const resumeInfo : ResumeInfo = {
@@ -56,8 +66,14 @@ const parseForm = () => {
   // Grab inputs from the form and append values to resumeInfo. Ignores the submit button (A little janky, come back to fix later)
   const inputs = document.querySelectorAll('input')
   for (const input in inputs) {
+    // skip the submit button since it's not an input we want to parse into our resumeInfo object
     if (inputs[input].type !== 'submit' && hasKey(resumeInfo, inputs[input].name)) {
       const name = inputs[input].name
+      let value = resumeInfo[name as keyof ResumeInfo]
+      // trim of any excess spaces
+      if (typeof value === 'string') {
+        value = value.trim()
+      }
       resumeInfo[name as keyof ResumeInfo] = inputs[input].value
     } else {
       // return some error here
@@ -73,12 +89,12 @@ function hasKey<O> (obj : O, key : PropertyKey) : key is keyof O {
 }
 
 // Event handlers:
-
 const handleSaveButton = (event : Event) => {
   // Parse each input and...
   const resumeInfo = parseForm()
   // Save info to sync storage API
   saveInfo(resumeInfo)
+  console.log(resumeInfo)
   // Display onto popup.html
   displayInfo(resumeInfo, () => generateInfoHTML(resumeInfo, 'info'))
   changeCornerButton('edit')
@@ -86,35 +102,24 @@ const handleSaveButton = (event : Event) => {
 
 const handleCloseButton = async (event : Event) => {
   const resumeInfo = await getInfo()
-  displayInfo(resumeInfo, () => generateInfoHTML(resumeInfo, 'info'))
-  changeCornerButton('edit')
+  await displayInfo(resumeInfo, () => generateInfoHTML(resumeInfo, 'info'))
+  await changeCornerButton('edit')
 }
 
 const handleEditButton = async (event : Event) => {
   const resumeInfo = await getInfo()
-  displayInfo(resumeInfo, () => generateFormHTML(resumeInfo, 'form'))
-  changeCornerButton('close')
+  await displayInfo(resumeInfo, () => generateFormHTML(resumeInfo, 'form'))
+  await changeCornerButton('close')
 }
+// ---------------------------------------------------------------------------------
 
-const displayInfo = (resumeInfo : ResumeInfo, generateFn : () => HTMLElement) => {
+const displayInfo = async (resumeInfo : ResumeInfo, generateFn : () => HTMLElement) => {
   const main = document.getElementById('main')
   if (main === null) {
     // error
     return
   }
 
-  // Double checking to see if this entries exists, otherwise polyfills it.
-  if (!Object.entries) {
-    Object.entries = function (obj : any) {
-      const ownProps = Object.keys(obj)
-      let i = ownProps.length
-      const resArray = new Array(i) // preallocate the Array
-      while (i--) {
-        resArray[i] = [ownProps[i], obj[ownProps[i]]]
-      }
-      return resArray
-    }
-  }
   main.firstElementChild?.remove()
   const container = generateFn()
   main.appendChild(container)
@@ -176,7 +181,7 @@ const generateInfoHTML = (resumeInfo : ResumeInfo, containerId : string) => {
   return container
 }
 
-const changeCornerButton = (action : string) => {
+const changeCornerButton = async (action : string) => {
   const button = document.getElementById('cornerButton')
   if (button === null) throw Error('cornerbutton is null')
   button.innerHTML = action
@@ -196,3 +201,43 @@ const changeCornerButton = (action : string) => {
     default : console.log('error')
   }
 }
+
+const init = async () => {
+  console.log('calling init...')
+  if (save !== null) {
+    save.onclick = (event: Event) => {
+      event.preventDefault()
+      handleSaveButton(event)
+    }
+  }
+  if (cornerButton !== null) {
+    cornerButton.onclick = (event: Event) => {
+      event.preventDefault()
+      handleCloseButton(event)
+    }
+  }
+
+  // Double checking to see if this entries exists, otherwise polyfills it.
+  if (!Object.entries) {
+    Object.entries = function (obj : any) {
+      const ownProps = Object.keys(obj)
+      let i = ownProps.length
+      const resArray = new Array(i) // preallocate the Array
+      while (i--) {
+        resArray[i] = [ownProps[i], obj[ownProps[i]]]
+      }
+      return resArray
+    }
+  }
+
+  // 0: parse info from storage
+  getInfo().then(res => {
+    console.log('res : ')
+    console.log(res)
+    parseInfo(res)
+  }).catch(rej => {
+    return rej
+  })
+}
+
+init()
